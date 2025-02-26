961ff122fa4827a3b19ccdceb88598079307d455
MichaelConn27 <michael.connors@student.fairfield.edu>
2025-02-26 14:55:19 -0500

    PhonebookEntry inital constructer and methods

--- PhonebookEntry.java
+++ PhonebookEntry.java
@@ -10,6 +10,26 @@ package template;
  */
 
 class PhonebookEntry{
+	
+	private int number;
+	private String type;
+	
+	public PhonebookEntry(int number String numberType) {
+		this.number = number;
+		this.type = numberType;
+	}
+	
+	public String getNumberType() {
+		return type;
+	}
+	
+	public int getPhoneNumber() {
+		return number;
+	}
+	
+	public boolean updateNumber(int number) {
+		this.number = number;
+	}
 
  
 }
